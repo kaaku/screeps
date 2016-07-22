4e0fcd5c7805d68c44550c1b58fb0dbfebd4e42f
Markus Yrjola <yrjola.markus@gmail.com>
2016-07-22 16:18:02 +0300

    Changed the way tower targeting works. There's no maximum range anymore, and new targets are chosen based on range only

--- src/controller.tower.js
+++ src/controller.tower.js
@@ -18,24 +18,24 @@ module.exports = {
         var target = Game.getObjectById(tower.getFromMemory('attackTargetId'));
         if (target && (target.canAttack() || target.canHeal())) {
             return target;
-        } else {
-            // TODO: Come up with a formula to wage between distance and remaining health of the targets
-            var attackTargets = tower.pos.findInRange(FIND_HOSTILE_CREEPS, 10, {
-                filter: creep => creep.canAttack() || creep.canHeal()
-            });
-            target = _.first(_.sortBy(attackTargets, function (target) {
-                // TODO: Is hit points a good selection criteria? What about e.g. damage output?
-                return target.hits / target.hitsMax;
-            }));
-
-            if (target) {
-                console.log(`Found new attack target at (${target.pos.x}, ${target.pos.y}) with body ${_.map(target.body, 'type')}, gunning down!`);
-                tower.setToMemory('attackTargetId', target.id);
-            } else {
-                tower.clearFromMemory('attackTargetId');
-            }
-
-            return target;
         }
+
+        var hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
+        if (_.isEmpty(hostiles)) {
+            target = null;
+        } else {
+            var offensiveHostiles = _.filter(hostiles, creep => creep.canAttack() || creep.canHeal());
+            target = _.isEmpty(offensiveHostiles) ?
+                    tower.pos.findClosestByRange(hostiles) : tower.pos.findClosestByRange(offensiveHostiles);
+        }
+
+        if (target) {
+            console.log(`Found new attack target at (${target.pos.x}, ${target.pos.y}) with body ${_.map(target.body, 'type')}, gunning down!`);
+            tower.setToMemory('attackTargetId', target.id);
+        } else {
+            tower.clearFromMemory('attackTargetId');
+        }
+
+        return target;
     }
 };
